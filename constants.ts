1883a0b64bff387a3360b9ee51e457d2ca5d989c
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-07-26 11:06:45 +0100

    Correct mistakes

--- constants.ts
+++ constants.ts
@@ -1,11 +1,15 @@
-// There's three ways to define a constant in JS. Write them down below and explain the differences
+/**
+ * There's three ways to define a constant in JS. Write them down below and explain the differences
+ */
 
-var perrito;                    //It can be update and re-declarate in the scope. 
+var perrito;
+//It can be update and re-declare in the scope. 
 //Also it has a functional scope.
 
-let perrita;                    //It can be update but no re-declarate in the scope.
+let perrita;
+//It can be update but no re-declare in the scope.
 //The scope of let variable is block scope.
 
-const cachorro = 'tornillo';    //It cannot be update nor re-decalrate in the scope
+const cachorro = 'tornillo';    //It cannot be update nor re-declared in the scope
                                 //The scope of const is block scope.
-                                //Also it has to be initializated when it's declarated.
+                                //Also it has to be initializated when it's declare.
