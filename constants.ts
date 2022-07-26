9c1e3688e27da853c9efc745e8e3fbc577300cb0
EduDG98 <90718854+EduDG98@users.noreply.github.com>
2022-07-26 09:50:58 +0100

    Study variales

--- constants.ts
+++ constants.ts
@@ -1,1 +1,5 @@
 // There's three ways to define a constant in JS. Write them down below and explain the differences
+
+var perrito;                    //The value of the var can't be modified
+let perrita;                    //For variables whichs values will be update
+const cachorro = 'tornillo';    //The value can be also update
